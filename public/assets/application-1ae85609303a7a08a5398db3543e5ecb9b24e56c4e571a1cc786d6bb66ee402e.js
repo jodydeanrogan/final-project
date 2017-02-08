536962d08c0fa8aed48d38f9078ce28de0fab96a
jodydeanrogan <jodydeanrogan@yahoo.co.uk>
2017-02-08 16:48:32 +0000

    clobbered and precompiled

--- public/assets/application-1ae85609303a7a08a5398db3543e5ecb9b24e56c4e571a1cc786d6bb66ee402e.js
+++ public/assets/application-1ae85609303a7a08a5398db3543e5ecb9b24e56c4e571a1cc786d6bb66ee402e.js
@@ -14452,7 +14452,7 @@ var widget = $.widget;
 
 
 }).call(this);
-$(document).ready(function() {
+$(document).on("turbolinks:load", function() {
   var images = ['jumbotron-1.jpg', 'jumbotron-2.jpg', 'jumbotron-3.jpg'];
   $('.jumbotron').css({'background-image': 'url(/assets/jumbotron/' + images[Math.floor(Math.random() * images.length)] + ')'});
 });
