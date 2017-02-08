5ae7629313975ce84c0364baa52bcbbe85424217
jodydeanrogan <jodydeanrogan@yahoo.co.uk>
2017-02-08 16:47:37 +0000

    changes made to js for heroku

--- app/assets/javascripts/jumbotron.js
+++ app/assets/javascripts/jumbotron.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).on("turbolinks:load", function() {
   var images = ['jumbotron-1.jpg', 'jumbotron-2.jpg', 'jumbotron-3.jpg'];
   $('.jumbotron').css({'background-image': 'url(/assets/jumbotron/' + images[Math.floor(Math.random() * images.length)] + ')'});
 });
